e6ef98a9ba70fe01afa9f315f6ad77947f8d38e2
CarolinaGomezM <89950310+CarolinaGomezM@users.noreply.github.com>
2022-10-07 14:02:30 -0500

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -64,22 +64,22 @@ class Inventario{
             let tamanio = this.inventario.length;
             let prueba = 0;
             let aux = this.inventario[this.inventario.length-1];
+            if(producto.obtenerCodigo() < this.inventario[0].obtenerCodigo()){
+              this.inventario.push(aux);
+              for(let j = this.inventario.length-1; j > 0; j--){
+                  this.inventario[j] = this.inventario[j-1];
+              }
+              this.inventario[0] = producto;
+              return true;
+          }
+            if(producto.obtenerCodigo() > this.inventario[tamanio-1].obtenerCodigo()){
+                  this.inventario.push(producto);
+                  prueba = 1;
+                 
+          }
             for(let i = 0; i < tamanio; i++){
-          
-              if(producto.obtenerCodigo() < this.inventario[0].obtenerCodigo()){
-                this.inventario.push(aux);
-                for(let j = this.inventario.length-1; j < 0; j--){
-                    this.inventario[j] = this.inventario[j-1];
-                }
-                this.inventario[0] = producto;
-            }
-              if(producto.obtenerCodigo() > this.inventario[tamanio-1].obtenerCodigo() && prueba == 0){
-                    this.inventario.push(producto);
-                    prueba = 1;
-                   
-            }
             if(producto.obtenerCodigo() > this.inventario[i].obtenerCodigo() && producto.obtenerCodigo() < this.inventario[i+1].obtenerCodigo()){
-              for(let j = this.inventario.length-1; j < i; j--){
+              for(let j = this.inventario.length-1; j >=i+1; j--){
                   this.inventario[j] = this.inventario[j-1];
               }
               this.inventario.push(aux);
